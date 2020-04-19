9defbf36ccb7b08454d071f5cfda2e359b87e2cf
Josh Bruce <josh@8fold.pro>
2020-04-19 13:06:26 -0500

    Update Shoop reference

--- src/uikit/Elements/Compound/Markdown.php
+++ src/uikit/Elements/Compound/Markdown.php
@@ -4,7 +4,11 @@ namespace Eightfold\Markup\UIKit\Elements\Compound;
 
 use Eightfold\Markup\Html\Elements\HtmlElement;
 
-use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Converter;
+use League\CommonMark\DocParser;
+use League\CommonMark\Environment;
+use League\CommonMark\HtmlRenderer;
+use League\CommonMark\Extension\Table\TableExtension;
 
 use Eightfold\Shoop\Shoop;
 
@@ -27,7 +31,19 @@ class Markdown extends HtmlElement
 
     public function unfold(): string
     {
+        // Obtain a pre-configured Environment with all the standard CommonMark parsers/renderers ready-to-go
+        $environment = Environment::createCommonMarkEnvironment();
+
+        // Add this extension
+        $environment->addExtension(new TableExtension());
+
+        // Instantiate the converter engine and start converting some Markdown!
+        $converter = new Converter(new DocParser($environment), new HtmlRenderer($environment));
+
+        return $converter->convertToHtml($this->markdown);
+
         $converter = new CommonMarkConverter($this->config);
+
         return $converter->convertToHtml($this->markdown);
     }
 }
