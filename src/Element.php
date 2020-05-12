7c9bc0f824a4df867aa047749506730ba762ae9a
Josh Bruce <josh@8fold.pro>
2020-05-12 18:22:10 -0500

    "Modernized" Element, HtmlElement  - deprecated AtriibuteHandler

--- src/Element.php
+++ src/Element.php
@@ -12,13 +12,11 @@ use Eightfold\Shoop\{
 
 class Element
 {
-    const openingFormat = "<%s%s>";
+    protected $element;
 
-    protected $element = '';
+    protected $content;
 
-    protected $content = [];
-
-    protected $extends = '';
+    protected $extends;
 
     protected $omitEndTag = false;
 
@@ -32,70 +30,41 @@ class Element
     public function __construct($element, ...$content)
     {
         $this->element = Type::sanitizeType($element, ESString::class)
-            ->replace("_", "-")->unfold();
-        $this->content = Type::sanitizeType($content, ESArray::class)
-            ->unfold();
+            ->replace("_", "-");
+        $this->content = Type::sanitizeType($content, ESArray::class);
     }
 
     public function unfold()
     {
-        $elem = $this->element;
-        if (strlen($this->extends) > 0) {
-            $elem = $this->extends;
-            $this->attr("is {$this->element}");
-        }
+        return Shoop::string($this->compiledElement())->start("<")->plus(
+            $this->compiledAttributes()
+        )->end(">")->plus(
+            $this->content->each(function($value) {
+                if (is_string($value) || is_int($value)) {
+                    return (string) $value;
 
-        // $attributes = Shoop::dictionary($this->attributes)
-        //     ->each(function($value, $key) {
-        //         var_dump($value);
-        //         var_dump($key);
-        //         return "{$key}=\"{$value}\"";
-        //     })->join(" ")->unfold();
-        // var_dump($attributes);
+                } elseif (is_a($value, Element::class) || is_subclass_of($value, Element::class)) {
+                    return $value->unfold();
 
-        $attributes = Shoop::array([]);
-        foreach ($this->attributes as $key => $value) {
-            // TODO: Write test regarding single attributes like required
-            if ($key === $value && strlen($value) > 0) {
-                $attributes = $attributes->plus($value);
-
-            } else {
-                $attributes = $attributes->plus("{$key}=\"{$value}\"");
-
-            }
-        }
-        $attributes = $attributes->join(" ");
-        if (strlen($attributes) > 0) {
-            $attributes = $attributes->start(" ");
-        }
-
-        $opening = "<{$elem}{$attributes}>";
-
-        $content = "";
-        foreach ($this->content as $value) {
-            if (is_string($value) || is_int($value)) {
-                $content .= (string) $value;
-
-            } elseif (is_a($value, Element::class) || is_subclass_of($value, Element::class)) {
-                $content .= $value->unfold();
-
-            }
-        }
-
-        $closing = "</{$elem}>";
-        if ($this->omitEndTag) {
-            $closing = "";
-        }
-        return $opening . $content . $closing;
+                }
+            })->join("")
+        )->plus(
+            ($this->omitEndTag) ? "" : Shoop::string($this->compiledElement())->start("</")->end(">")
+        )->unfold();
     }
 
     public function attr(string ...$attributes): Element
     {
-        foreach ($attributes as $attribute) {
-            if (strlen($attribute) > 0) {
-                list($key, $value) = explode(" ", $attribute, 2);
-                $this->attributes[$key] = $value;
-            }
+        if ($this->attributes === null) {
+            $this->attributes = Shoop::array($attributes)->unfold();
+
+        } else {
+            $current = $this->attributes;
+            $new = $attributes;
+            $merged = array_merge($current, $new);
+            $unique = array_unique($merged);
+            $this->attributes = Shoop::array($unique)->unfold();
+
         }
         return $this;
     }
@@ -111,6 +80,30 @@ class Element
     {
         $this->omitEndTag = $omit;
         return $this;
+    }
+
+    protected function compiledElement()
+    {
+        $elem = $this->element;
+        if (strlen($this->extends) > 0) {
+            $elem = $this->extends;
+            $this->attributes[] = "is {$this->element}";
+        }
+        return $elem;
+    }
+
+    protected function compiledAttributes(): string
+    {
+        $compiled = Shoop::array($this->attributes)->each(function($attribute) {
+            list($member, $value) = explode(" ", $attribute, 2);
+            return ($member === $value && strlen($member) > 0)
+                ? $member : "{$member}=\"{$value}\"";
+        });
+
+        if ($compiled->int()->isGreaterThanUnfolded(0)) {
+            return $compiled->join(" ")->start(" ");
+        }
+        return "";
     }
 
     public function __toString()
