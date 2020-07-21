d1be77246874ac0ff70c1100acbd422025cb9a23
Josh Bruce <josh@8fold.pro>
2020-07-20 20:06:04 -0500

    fixes

--- tests/UIKit/SocialMetaTest.php
+++ tests/UIKit/SocialMetaTest.php
@@ -12,7 +12,6 @@ class SocialMetaTest extends TestCase
     {
         $expected = '<meta content="website" property="og:type"><meta content="Hello, World!" property="og:title"><meta content="https://8fold.pro" property="og:url"><meta content="A short description. LinkedIn would like to have 100+ characters." property="og:description">';
         $actual = UIKit::socialMeta(
-            "website",
             "Hello, World!",
             "https://8fold.pro",
             "A short description. LinkedIn would like to have 100+ characters."
@@ -21,11 +20,11 @@ class SocialMetaTest extends TestCase
 
         $expected = '<meta content="website" property="og:type"><meta content="Hello, World!" property="og:title"><meta content="https://8fold.pro" property="og:url"><meta content="A short description. LinkedIn would like to have 100+ characters." property="og:description"><meta content="https://8fold.pro/assets/ui/logo.svg" property="og:image"><meta content="ABCDEFGHIJKLMNOP" property="og:app_id">';
         $actual = UIKit::socialMeta(
-            "website",
             "Hello, World!",
             "https://8fold.pro",
             "A short description. LinkedIn would like to have 100+ characters.",
             "https://8fold.pro/assets/ui/logo.svg",
+            "website",
             "ABCDEFGHIJKLMNOP"
         );
         $this->assertEquals($expected, $actual->unfold());
@@ -35,11 +34,11 @@ class SocialMetaTest extends TestCase
     {
         $expected = '<meta content="website" property="og:type"><meta content="Hello, World!" property="og:title"><meta content="https://8fold.pro" property="og:url"><meta content="A short description. LinkedIn would like to have 100+ characters." property="og:description"><meta content="https://8fold.pro/assets/ui/logo.svg" property="og:image"><meta content="ABCDEFGHIJKLMNOP" property="og:app_id"><meta name="twitter:card" content="summary_large_image">';
         $actual = UIKit::socialMeta(
-            "website",
             "Hello, World!",
             "https://8fold.pro",
             "A short description. LinkedIn would like to have 100+ characters.",
             "https://8fold.pro/assets/ui/logo.svg",
+            "website",
             "ABCDEFGHIJKLMNOP"
         )->twitter();
         $this->assertEquals($expected, $actual->unfold());
