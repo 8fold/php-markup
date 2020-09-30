687000fc382841e703ca0927e475d20078e4b297
Josh Bruce <josh@8fold.pro>
2020-09-30 14:25:45 -0500

    social meta tests passing

--- tests/UIKit/SocialMetaTest.php
+++ tests/UIKit/SocialMetaTest.php
@@ -3,12 +3,13 @@
 namespace Eightfold\Markup\Tests\UIKit;
 
 use PHPUnit\Framework\TestCase;
-use Eightfold\Foldable\Tests\TestEqualsPerformance as AssertEquals;
+use Eightfold\Foldable\Tests\PerformantEqualsTestFilter as AssertEquals;
 
 use Eightfold\Markup\UIKit;
 
 /**
  * @group SocialMeta
+ * @group 1.0.0
  */
 class SocialMetaTest extends TestCase
 {
@@ -20,7 +21,8 @@ class SocialMetaTest extends TestCase
         AssertEquals::applyWith(
             '<meta content="website" property="og:type"><meta content="Hello, World!" property="og:title"><meta content="https://8fold.pro" property="og:url"><meta content="A short description. LinkedIn would like to have 100+ characters." property="og:description">',
             "string",
-            10.77 // 9.72
+            11.17, // 9.8, // 9.51,
+            552
         )->unfoldUsing(
             UIKit::socialMeta(
                 "Hello, World!",
@@ -32,7 +34,8 @@ class SocialMetaTest extends TestCase
         AssertEquals::applyWith(
             '<meta content="website" property="og:type"><meta content="Hello, World!" property="og:title"><meta content="https://8fold.pro" property="og:url"><meta content="A short description. LinkedIn would like to have 100+ characters." property="og:description"><meta content="https://8fold.pro/assets/ui/logo.svg" property="og:image"><meta content="ABCDEFGHIJKLMNOP" property="og:app_id">',
             "string",
-            5.86 // 5.47 // 5.43
+            5.93, // 5.54,
+            1
         )->unfoldUsing(
             UIKit::socialMeta(
                 "Hello, World!",
@@ -53,7 +56,8 @@ class SocialMetaTest extends TestCase
         AssertEquals::applyWith(
             '<meta content="website" property="og:type"><meta content="Hello, World!" property="og:title"><meta content="https://8fold.pro" property="og:url"><meta content="A short description. LinkedIn would like to have 100+ characters." property="og:description"><meta content="https://8fold.pro/assets/ui/logo.svg" property="og:image"><meta content="ABCDEFGHIJKLMNOP" property="og:app_id"><meta name="twitter:card" content="summary_large_image">',
             "string",
-            13.43
+            7.77, // 6.73, // 6.54,
+            1
         )->unfoldUsing(
             UIKit::socialMeta(
                 "Hello, World!",
