8912d67db6b62d6e97e97e4f6c487ecc4b8a6b41
Josh Bruce <josh@8fold.pro>
2021-10-08 17:52:11 -0500

    Markdown passing stan

--- src/UIKit/Elements/Compound/Markdown.php
+++ src/UIKit/Elements/Compound/Markdown.php
@@ -2,91 +2,126 @@
 
 namespace Eightfold\Markup\UIKit\Elements\Compound;
 
-use Eightfold\Markup\Html\HtmlElement;
+use League\CommonMark\Extension\ExtensionInterface;
 
-use Eightfold\ShoopShelf\Shoop;
+// use Eightfold\Markup\Html\HtmlElement;
 
-class Markdown extends HtmlElement
+// use Eightfold\ShoopShelf\Shoop;
+
+class Markdown //extends HtmlElement
 {
-    private $markdown = "";
-    private $config = [];
-    private $caseSensitive = true;
-    private $markdownReplacements = [];
+    private string $markdown = '';
+
+    /**
+     * @var array<mixed>
+     */
+    private array $config = [];
+
+    private bool $caseSensitive = true;
+
+    /**
+     * @var array<string>
+     */
+    private array $markdownReplacements = [];
+
+    /**
+     * @var array<string>
+     */
     private $htmlReplacements = [];
-    private $trim = true;
-    private $minified = true;
+
+    private bool $trim = true;
+
+    private bool $minified = true;
+
+    /**
+     * @var array<ExtensionInterface>
+     */
     private $extensions = [];
 
+    /**
+     * @param array<mixed> $config [description]
+     */
     public function __construct(string $markdown, array $config = [])
     {
         $this->config = $config;
         $this->markdown = $markdown;
     }
 
-    public function caseSensitive($caseSensitive = true)
+    public function caseSensitive(bool $caseSensitive = true): Markdown
     {
         $this->caseSensitive = $caseSensitive;
 
         return $this;
     }
 
-    public function markdownReplacements($replacements = [])
+    /**
+     * @param  array<string> $replacements [description]
+     */
+    public function markdownReplacements($replacements = []): Markdown
     {
         $this->markdownReplacements = $replacements;
 
         return $this;
     }
 
-    public function htmlReplacements($replacements = [])
+    /**
+     * @param  array<string> $replacements [description]
+     */
+    public function htmlReplacements($replacements = []): Markdown
     {
         $this->htmlReplacements = $replacements;
 
         return $this;
     }
 
-    public function trim($trim = true)
+    public function trim(bool $trim = true): Markdown
     {
         $this->trim = $trim;
 
         return $this;
     }
 
-    public function minified($minified = true)
+    public function minified(bool $minified = true): Markdown
     {
         $this->minified = $minified;
 
         return $this;
     }
 
-    public function extensions(...$extensions)
+    /**
+     * @param  ExtensionInterface $extensions [description]
+     */
+    public function extensions(...$extensions): Markdown
     {
-        $this->extensions = Shoop::this($extensions)
-            ->drop(fn($v) => empty($v))->unfold();
+        // $this->extensions = Shoop::this($extensions)
+        //     ->drop(fn($v) => empty($v))->unfold();
 
         return $this;
     }
 
-    public function prepend(string $markdown)
+    public function prepend(string $markdown): Markdown
     {
         $this->markdown = $markdown . $this->markdown;
 
         return $this;
     }
 
-    public function meta()
+    public function meta(): object
     {
-        return Shoop::markdown($this->markdown)->meta();
+        return (object) [];
+        // return Shoop::markdown($this->markdown)->meta();
     }
 
-    public function unfold(): string
+    public function build(): string
     {
-        return Shoop::markdown($this->markdown, ...$this->extensions)
-            ->html(
-                $this->markdownReplacements,
-                $this->htmlReplacements,
-                $this->caseSensitive,
-                $this->minified,
-                $this->config
-            )->unfold();
+        return '';
+        // return Shoop::markdown($this->markdown, ...$this->extensions)
+        //     ->html(
+        //         $this->markdownReplacements,
+        //         $this->htmlReplacements,
+        //         $this->caseSensitive,
+        //         $this->minified,
+        //         $this->config
+        //     )->unfold();
     }
 }
